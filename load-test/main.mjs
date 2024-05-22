11a9214f624931bed648c325a93842941962b750
Matthew Bridges <mattbridgesbusiness@gmail.com>
2024-05-22 12:15:47 -0700

    remove comments from load test

--- load-test/main.mjs
+++ load-test/main.mjs
@@ -1,6 +1,5 @@
 import fetch from "node-fetch";
 
-// write a generic `request` function using node-fetch, return all responses, do not throw >= 400, assume application/json always
 async function request(url, method, body) {
   const response = await fetch(url, {
     method,
@@ -18,13 +17,11 @@ async function request(url, method, body) {
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function main() {
-  // fire requests constantly for 60 seconds, store bad requests in one array, good requests in another
   const badRequests = [];
   const goodRequests = [];
   const startTime = Date.now();
   const endTime = startTime + 60000;
   while (Date.now() < endTime) {
-    // make GET request to http://localhost:6666/api/hello", if status code == 200, store in goodRequests, if 429 store in badRequests
     const response = await request("http://localhost:6666/api/hello", "GET");
     if (response.statusCode === 200) {
       goodRequests.push(response);
